d22e45c1268e2bf40b9eff953fb92591ecbb03ee
chris924 <burkakrisztofer@hotmail.com>
2023-11-28 22:08:52 +0100

    feat: adds entities

---
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/entity/Company.java
@@ -0,0 +1,26 @@
+package com.example.bookitnowbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "COMPANY_TBL")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String companyName;
+
+    private String description;
+
+    private Timestamp createdAt;
+}
