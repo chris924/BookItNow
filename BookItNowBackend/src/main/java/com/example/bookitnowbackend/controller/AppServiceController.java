0b7749d362ab5687ea725dde5f4cbd8cb8f3ba04
chris924 <burkakrisztofer@hotmail.com>
2023-12-01 19:49:35 +0100

    feat: CompanyController.java, UserController.java has error handling

--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/AppServiceController.java
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/AppServiceController.java
@@ -31,7 +31,7 @@ public class AppServiceController {
             AppService savedAppService = appServiceService.saveService(appService);
             return ResponseEntity.status(HttpStatus.CREATED).body(savedAppService);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding app service");
         }
     }
 
--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/CompanyController.java
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/CompanyController.java
@@ -2,10 +2,15 @@ package com.example.bookitnowbackend.controller;
 
 import com.example.bookitnowbackend.entity.Company;
 import com.example.bookitnowbackend.service.CompanyService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class CompanyController {
@@ -15,33 +20,69 @@ public class CompanyController {
     private CompanyService companyService;
 
     @PostMapping("/addCompany")
-    public Company addCompany(@RequestBody Company company)
+    public ResponseEntity<?> addCompany(@Valid @RequestBody Company company, BindingResult bindingResult)
     {
-        return companyService.saveCompany(company);
+        if(bindingResult.hasErrors())
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request");
+        }
+        try {
+            Company savedCompany = companyService.saveCompany(company);
+            return ResponseEntity.status(HttpStatus.OK).body(savedCompany);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding company");
+        }
     }
 
     @GetMapping("/getAllCompanies")
-    public List<Company> getAllCompanies()
+    public ResponseEntity<?> getAllCompanies()
     {
-        return companyService.getCompanies();
+        try {
+            List<Company> companies = companyService.getCompanies();
+            return ResponseEntity.status(HttpStatus.OK).body(companies);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error getting companies");
+        }
     }
 
     @GetMapping("/getCompanyById/{id}")
-    public Company getCompanyById(@PathVariable Integer id)
+    public ResponseEntity<?> getCompanyById(@PathVariable Integer id)
     {
-        return companyService.getCompanyById(id);
+        try {
+            Company companyById = companyService.getCompanyById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(companyById);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error getting company by id");
+        }
     }
 
     @PutMapping("/updateCompany")
-    public Company updateCompany(@RequestBody Company company)
+    public ResponseEntity<?> updateCompany(@Valid @RequestBody Company company, BindingResult bindingResult)
     {
-        return companyService.updateCompany(company);
+       if(bindingResult.hasErrors())
+       {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Invalid request");
+       }
+       try {
+           Company updatedCompany = companyService.updateCompany(company);
+           return ResponseEntity.status(HttpStatus.OK).body(updatedCompany);
+       }catch (Exception e){
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating company");
+       }
     }
 
     @DeleteMapping("/deleteCompany/{id}")
-    public String deleteCompany(@PathVariable Integer id)
+    public ResponseEntity<?> deleteCompany(@PathVariable Integer id)
     {
-        return companyService.deleteCompany(id);
+        try {
+            String deletedCompany = companyService.deleteCompany(id);
+            return ResponseEntity.status(HttpStatus.OK).body(deletedCompany);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting company");
+        }
+
+
+
     }
 
 
