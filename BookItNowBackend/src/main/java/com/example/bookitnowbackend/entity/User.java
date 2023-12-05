cf65c8676c27a18710f8fb10d3b4dcd99265af6b
chris924 <burkakrisztofer@hotmail.com>
2023-12-05 19:03:51 +0100

    fix: modified User.java for auth

--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/entity/User.java
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/entity/User.java
@@ -4,18 +4,24 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "USER_TBL")
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_id")
     private Integer id;
 
     private String name;
@@ -31,5 +37,77 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Appointment> appointments;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role_junction",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn (name = "role_id")}
+    )
+    private Set<Role> authorities;
 
+
+    public Integer getUserId()
+    {
+        return this.id;
+    }
+
+    public void setUserId(Integer id)
+    {
+        this.id = id;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public void setAuthorities(Set<Role> authorities)
+    {
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
