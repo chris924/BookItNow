adff80af6eae0cd9611c475dbc49c4ff82218537
chris924 <burkakrisztofer@hotmail.com>
2023-12-05 18:26:23 +0100

    feat: created UserController.java

--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/UserController.java
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/controller/UserController.java
@@ -12,72 +12,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+   @GetMapping("/")
+    public String helloUserController()
+   {
+       return "User access level";
+   }
 
-    @PostMapping("/addUser")
-    public ResponseEntity<?> addUser(@Valid @RequestBody User user, BindingResult bindingResult)
-    {
-        if(bindingResult.hasErrors())
-        {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request");
-        }
-        try {
-            User savedUser = userService.saveUser(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding user");
-        }
-    }
 
-    @GetMapping("/getAllUsers")
-    public ResponseEntity<?> getAllUsers()
-    {
-        try {
-            List<User> allUsers = userService.getUsers();
-            return ResponseEntity.status(HttpStatus.OK).body(allUsers);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error getting users");
-        }
-    }
-
-    @GetMapping("/getUserById/{id}")
-    public ResponseEntity<?> findUserById(@PathVariable Integer id)
-    {
-        try {
-            User userById = userService.getUserById(id);
-            return ResponseEntity.status(HttpStatus.OK).body(userById);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error getting user by id");
-        }
-    }
-
-    @PutMapping("/updateUser")
-    public ResponseEntity<?> updateUser(@Valid @RequestBody User user, BindingResult bindingResult)
-    {
-        if(bindingResult.hasErrors())
-        {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request");
-        }
-
-        try {
-            User updatedUser = userService.updateUser(user);
-            return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating user");
-        }
-    }
-
-    @DeleteMapping("/deleteUser/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Integer id)
-    {
-        try {
-            String deletedUser = userService.deleteUser(id);
-            return ResponseEntity.status(HttpStatus.OK).body(deletedUser);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting user");
-        }
-    }
 }
