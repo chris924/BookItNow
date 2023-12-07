a74ad106a240dc75cd7e65fec9ba1197e0206da1
chris924 <burkakrisztofer@hotmail.com>
2023-12-07 22:09:51 +0100

    feat: adds LoginResponseDTO for AuthenticationService.java

--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/service/AuthenticationService.java
+++ BookItNowBackend/src/main/java/com/example/bookitnowbackend/service/AuthenticationService.java
@@ -1,11 +1,16 @@
 package com.example.bookitnowbackend.service;
 
 import com.example.bookitnowbackend.entity.Appointment;
+import com.example.bookitnowbackend.entity.LoginResponseDTO;
 import com.example.bookitnowbackend.entity.Role;
 import com.example.bookitnowbackend.entity.User;
 import com.example.bookitnowbackend.repository.IRoleRepository;
 import com.example.bookitnowbackend.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +31,13 @@ public class AuthenticationService {
     private IRoleRepository roleRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
 
     public User registerUser(String name, String username, String email, String password)
     {
@@ -49,6 +61,22 @@ public class AuthenticationService {
 
         return userRepository.save(newUser);
     }
+
+public LoginResponseDTO loginUser(String username, String password)
+{
+    try{
+
+        Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        String token = tokenService.generateJwt(auth);
+
+        return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+
+    }catch (AuthenticationException e) {
+        return new LoginResponseDTO(null, "");
+    }
+
+
+}
 
 
 
