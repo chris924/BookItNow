52d36d9aa7eb7c54c099dab291e2645d09db183c
chris924 <burkakrisztofer@hotmail.com>
2024-01-17 19:16:30 +0100

    feat: user appointment table works with real data

--- BookItNowBackend/src/main/java/com/example/bookitnowbackend/repository/IServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookitnowbackend.repository;
-
-import com.example.bookitnowbackend.entity.AppService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IServiceRepository extends JpaRepository<AppService, Integer> {
-
-}
